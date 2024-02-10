82b77506761d41e836556e7756ca97c7422aa2f5
Jahid Hasan <vdjsovaj@gmail.com>
2024-02-10 08:26:50 +0600

    update test

---
+++ test/features/now_playing_movies/domain/usecases/get_now_playing_movies_usecase_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_demo_app/core/resources/data_state.dart';
+import 'package:flutter_demo_app/features/now_playing_movies/domain/entities/now_playing_movies_entity.dart';
+import 'package:flutter_demo_app/features/now_playing_movies/domain/usecases/get_now_playing_movies_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/now_playing_movies_repository_helpers.mocks.dart';
+
+void main() {
+  late GetNowPlayingMoviesUseCase getNowPlayingMoviesUseCase;
+  late MockNowPlayingMoviesRepository mockNowPlayingMoviesRepository;
+
+  setUp(() {
+    mockNowPlayingMoviesRepository = MockNowPlayingMoviesRepository();
+    getNowPlayingMoviesUseCase =
+        GetNowPlayingMoviesUseCase(mockNowPlayingMoviesRepository);
+  });
+
+  final testMovieDetails = NowPlayingMoviesEntity(
+    adult: false,
+    backdropPath: '/test.jpg',
+    genreIds: const [1, 2],
+    id: 1,
+    originalLanguage: 'en',
+    originalTitle: 'Test Movie',
+    overview: 'Test Overview',
+    popularity: 1.0,
+    posterPath: '/test.jpg',
+    releaseDate: DateTime(2021, 1, 1),
+    title: 'Test Movie',
+    video: false,
+    voteAverage: 1.0,
+    voteCount: 1,
+  );
+
+  test('should get now playing movies from the repository', () async {
+    // arrange
+    when(mockNowPlayingMoviesRepository.getNowPlayingMovies())
+        .thenAnswer((_) async => DataSuccess([testMovieDetails]));
+    // act
+    final result = await getNowPlayingMoviesUseCase.call();
+    // assert
+    expect(result.data, [testMovieDetails]);
+  });
+}
---
+++ test/features/now_playing_movies/helpers/now_playing_movies_repository_helpers.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:flutter_demo_app/features/now_playing_movies/domain/repository/now_playing_movies_repository.dart';
+
+@GenerateMocks([NowPlayingMoviesRepository])
+void main() {}
